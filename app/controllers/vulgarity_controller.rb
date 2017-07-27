06f3e9e67fbb4af496f7d31ce77e464fa61f49f0
m0nfil <fabiolamonfil@gmail.com>
2017-07-27 12:23:12 -0500

    Testing Vulgarity query

--- app/controllers/vulgarity_controller.rb
+++ app/controllers/vulgarity_controller.rb
@@ -3,7 +3,8 @@ class VulgarityController < ApplicationController
 	def add
 		p "*** VULGARITY"
 		p current_user
-		p user_vulgarity = current_user.vulgarity
+		p Vulgarity.all
+		p user_vulgarity = Vulgarity.find_by(user_id: current_user.id) #current_user.vulgarity
 		p user_vulgarity.increment!(:count)
 		p user_vulgarity.update(amount: user_vulgarity.count * 5)
 		flash[:success] = "You have commited #{user_vulgarity.count} fouls and now, you have to pay $ #{user_vulgarity.amount}"
