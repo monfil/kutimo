906acb3524e14acc25c2574c0bf12bb9ea3d5c31
m0nfil <fabiolamonfil@gmail.com>
2017-07-27 12:40:05 -0500

    Vulgarity query modified

--- app/controllers/vulgarity_controller.rb
+++ app/controllers/vulgarity_controller.rb
@@ -4,7 +4,7 @@ class VulgarityController < ApplicationController
 		p "*** VULGARITY"
 		p current_user
 		p Vulgarity.all
-		p user_vulgarity = Vulgarity.find_by(user_id: current_user.id) #current_user.vulgarity
+		p user_vulgarity = current_user.vulgarity
 		p user_vulgarity.increment!(:count)
 		p user_vulgarity.update(amount: user_vulgarity.count * 5)
 		flash[:success] = "You have commited #{user_vulgarity.count} fouls and now, you have to pay $ #{user_vulgarity.amount}"
