a247550597a18613152bc7951e0d3bbe76e40158
m0nfil <fabiolamonfil@gmail.com>
2017-07-28 07:09:34 -0500

    Goals CRUD v 1

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -8,13 +8,69 @@ class GoalsController < ApplicationController
 		render 'goals_options'
 	end
 
+	def new
+		@new_goal = Goal.new
+	end
+
+	def create
+		@goal = Goal.new(goal_params)
+		if @goal.goal != ""
+			@goal.save
+			redirect_to goals_path
+		else
+			redirect_to new_goal_path
+		end
+	end
+
 	def destroy
-		p "*** DESTROY GOAL ***"
-		p params
-		p user_goal = UserGoal.find_by(user_id: params[:user_id], goal_id: params[:goal_id])
-		p @goal_id = params[:goal_id]
+		user_goal = UserGoal.find_by(user_id: params[:user_id], goal_id: params[:goal_id])
+		@goal_id = params[:goal_id]
 		user_goal.destroy
 	end
 
+	def edit
+		@goal_edit = Goal.find(params[:goal_id])
+	end
+
+	def update
+		@goal = Goal.find(params[:id])
+		if @goal.update(goal_params)
+			redirect_to user_goals_path
+		else
+			render 'edit'
+		end
+
+	end
+
+	def show_goals
+		@all_goals = Goal.all
+		@user_goals = current_user.goals
+		render 'goals_options'
+	end
+
+	def add_goal
+		add_goal = UserGoal.create(user_id: params[:user_id], goal_id: params[:goal_id])		
+	end
+
+	def restart_goal_record
+		user_goal = UserGoal.find_by(user_id: params[:user_id], goal_id: params[:goal_id])
+		goal_daily_records = user_goal.daily_records
+		goal_daily_records.delete_all
+	end
+
+	def mark_goal
+		user_goal = UserGoal.find_by(user_id: params[:user_id], goal_id: params[:goal_id])
+		DailyRecord.create(user_goal_id: user_goal.id)
+		goal_count = user_goal.daily_records.count
+		if goal_count >= 15
+			user_goal.update(accomplished: true)
+		end
+	end
+
+	private
+
+		def goal_params
+			params.require(:goal).permit([:goal])
+		end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -28,7 +28,23 @@ Rails.application.routes.draw do
 
 	get     '/user_goals', to: 'goals#user_goals', as: 'user_goals'
 
-	delete '/user_goals/:user_id/:goal_id', to: 'goals#destroy', as: 'goal'
+	get     '/new_goal',   to: 'goals#new', as: 'new_goal'
+
+	post     '/new_goal',  to: 'goals#create', as: 'create_goal'
+
+	delete  '/user_goals/:user_id/:goal_id', to: 'goals#destroy', as: 'goal'
+
+	get '/edit/:goal_id', to: 'goals#edit', as: 'edit_goal'
+
+	post '/update/:id', to: 'goals#update', as: 'update_goal'
+
+	get '/add_goal/:user_id', to: 'goals#show_goals', as: 'add_goal'
+
+	put '/add_goal_user/:user_id/:goal_id', to: 'goals#add_goal', as: 'add_goal_user'
+
+	delete '/restart_goal_record/:user_id/:goal_id', to: 'goals#restart_goal_record', as: 'restart_goal'
+
+	put '/mark_goal/:user_id/:goal_id', to: 'goals#mark_goal', as: 'goal_done'
 
 	resources :users
 
